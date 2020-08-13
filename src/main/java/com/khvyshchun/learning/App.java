bd3af4ac9cbdc00044346547880f4836c02d6f2d
Maksym <max.khvyshchun@gmail.com>
2020-08-13 11:22:42 +0300

    initial commit

---
+++ src/main/java/com/khvyshchun/learning/App.java
@@ -0,0 +1,13 @@
+package com.khvyshchun.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/khvyshchun/learning/loop/LoopApp.java
@@ -0,0 +1,19 @@
+package com.khvyshchun.learning.loop;
+
+public class LoopApp {
+    public static void main(String[] args) {
+        int iVar = 12;
+        short sVar = 23;
+        long lVar = 1234678866L;
+        byte bVar = 5;
+        double dVar = 1.4;
+        float fVar = 1.3f;
+        boolean boolVar = true;
+        String stringVar = "45";
+        char cVar = '2';
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println("number = "+i);
+        }
+    }
+}
